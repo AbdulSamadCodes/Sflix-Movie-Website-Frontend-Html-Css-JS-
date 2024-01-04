77d15e120d0879f6389381731031b54cdfd85170
Samaadcodes <samadcom797@gmail.com>
2024-01-04 23:13:26 +0500

    A frontend movie template

---
+++ script.js
@@ -0,0 +1,7 @@
+// Browse button dropdownmenu toggle functionality
+let browse = document.querySelector(".browse")
+let dropdownmenu  = document.querySelector(".dropdown-menu")
+browse.addEventListener("click",() => dropdownmenu.classList.toggle("toggle"))
+console.log(dropdownmenu)
+console.log(browse)
+
